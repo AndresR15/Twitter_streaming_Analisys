24aa7c53114f4668d303cfa796aee4b93c616b2d
Andres <andres15@my.yorku.ca>
2018-11-21 19:01:20 -0500

    started plotting, have to figure out get data from spark

---
+++ twitter_plot.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python 
+import sys, re, string, json
+
+import matplotlib.pyplot as plot
+import pandas
+
+# This is a basic listener that just prints received tweets to stdout.
+# Taken from http://adilmoujahid.com/posts/2014/07/twitter-analytics/
+
+
+def plot_titter_stats():
+	try:
+		for tweet in sys.stdin:
+			# for each tweet that comes in, json will parse it into the tweet_data list
+			try:
+				tweet_data = json.loads(tweet)
+				
+			except:
+				continue
+
+	except KeyboardInterrupt:
+		exit()
+
+if __name__ == "__main__": plot_titter_stats()		
